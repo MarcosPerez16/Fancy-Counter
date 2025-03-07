0b6bbb498382b30a74a29861b519d68e7d4a6f39
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-03-07 15:38:21 -0800

    Lifted state to our card component, then passed as props to our children components Count and CountButtons. Added logic to plus and mins buttons. Turned off react prop types warning.

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -1,13 +1,23 @@
 import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
-const CountButtons = () => {
+const CountButtons = ({ setCount }) => {
   return (
     <div className='button-container'>
       <button className='count-btn'>
-        <MinusIcon className='count-btn-icon' />
+        <MinusIcon
+          onClick={() => {
+            setCount((prev) => prev - 1);
+          }}
+          className='count-btn-icon'
+        />
       </button>
       <button className='count-btn'>
-        <PlusIcon className='count-btn-icon' />
+        <PlusIcon
+          onClick={() => {
+            setCount((prev) => prev + 1);
+          }}
+          className='count-btn-icon'
+        />
       </button>
     </div>
   );

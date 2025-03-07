0066837bdf8eb4921e7be4e7f472d2394fe2033e
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-03-07 15:03:07 -0800

    Created components, css file, and installed radix ui icons

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import Count from './Count';
+import CountButtons from './CountButtons';
+import ResetButton from './ResetButton';
+import Title from './Title';
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <Title />
+      <Count />
+      <ResetButton />
+      <CountButtons />
+    </div>
+  );
+};
+
+export default Card;

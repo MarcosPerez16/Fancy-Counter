19a938ffdfe968f45502125262f62604e44a74aa
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-03-07 16:56:53 -0800

    Added a locked limit to counter. Added logic for when the app locks, it changes the title.

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,22 +1,28 @@
 import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
-const CountButton = ({ type, setCount }) => {
+const CountButton = ({ type, setCount, locked }) => {
   const handleClick = () => {
     setCount((prev) => {
       if (type === 'minus') {
         const newCount = prev - 1;
+        // Prevent count from going below 0
         if (newCount < 0) {
           return 0;
         }
+        // Return the new count if it's greater than or equal to 0
         return newCount;
       } else {
-        return prev + 1;
+        const newCount = prev + 1;
+        if (newCount > 5) {
+          return 5;
+        }
+        return newCount;
       }
     });
   };
 
   return (
-    <button onClick={handleClick} className='count-btn'>
+    <button disabled={locked} onClick={handleClick} className='count-btn'>
       {type === 'minus' ? (
         <MinusIcon className='count-btn-icon' />
       ) : (

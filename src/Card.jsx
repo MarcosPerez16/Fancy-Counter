7b3f096d395cdb74737127b5803fc4bde1c4fbc6
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-03-11 17:40:40 -0700

    Used the children composition pattern to deal with prop drilling in our ButtonContainer component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Count from './Count';
 import ButtonContainer from './ButtonContainer';
+import CountButton from './CountButton';
 import ResetButton from './ResetButton';
 import Title from './Title';
 
@@ -25,12 +26,16 @@ const Card = () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [count]);
+
   return (
     <div className={`card ${locked ? 'card--limit' : ''}`}>
       <Title locked={locked} />
       <Count count={count} />
       <ResetButton setCount={setCount} />
-      <ButtonContainer setCount={setCount} locked={locked} />
+      <ButtonContainer>
+        <CountButton type='minus' setCount={setCount} locked={locked} />
+        <CountButton type='plus' setCount={setCount} locked={locked} />
+      </ButtonContainer>
     </div>
   );
 };
